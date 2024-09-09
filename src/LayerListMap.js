5a7a6e76716f4c60146dd31ce5ff7705ae5cbc86
CON Didit Praditya <GIS.Developer@dapeco.com.om>
2024-09-09 08:16:39 +0400

    updated to dpc web map

--- src/LayerListMap.js
+++ src/LayerListMap.js
@@ -3,15 +3,21 @@ import MapView from '@arcgis/core/views/MapView';
 import WebMap from '@arcgis/core/WebMap';
 import LayerList from '@arcgis/core/widgets/LayerList';
 import '@arcgis/core/assets/esri/themes/light/main.css';
+import esriConfig from "@arcgis/core/config"; 
 
 const LayerListMap = () => {
   const viewDiv = useRef(null); // Reference for the div element
 
   useEffect(() => {
     // Create the WebMap instance using the provided WebMap ID
+
+    esriConfig.portalUrl = "https://dpcgeo01.dapeco.com.om/portal/";
+    //esriConfig.portalUrl = "https://www.arcgis.com";
+
     const webmap = new WebMap({
       portalItem: {
-        id: 'e691172598f04ea8881cd2a4adaa45ba', // Your WebMap ID
+        //id: "18b13990505d4f0faf1158783846b78d"  // Your WebMap ID
+        id: "504d864dacfa4308998c124c496568eb"
       },
     });
 
